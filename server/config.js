1c7c93736817a75864ee7d6ae67dae6029551409
Brock Pettyjohn <brockpettyjohn@gmail.com>
2017-07-03 12:26:26 -0600

    minor changes

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    database: 'rtbyhlnb',
+    password: 'A0-eWWcExva1BJGSuCMOt7OvKyRvrqWN',
+    user: 'rtbyhlnb',
+    host: 'pellefant.db.elephantsql.com',
+    port: '5432',
+}
